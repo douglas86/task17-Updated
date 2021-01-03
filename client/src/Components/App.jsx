b04940a0f69fb7928ee9a02351b8d5cd6d476583
douglas86 <douglasmaxton@gmail.com>
2021-01-03 16:12:10 +0200

    Created a new Component Form.jsx

--- client/src/Components/App.jsx
+++ client/src/Components/App.jsx
@@ -1,10 +1,12 @@
 import '../extras/App.css';
+import Form from './Form';
 
 const App = () => {
     return (
         <div className="App">
             <header className="App-header">
                 <p>Home Page</p>
+                <Form />
             </header>
         </div>
     );
