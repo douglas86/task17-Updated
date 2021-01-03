1ee19a4882a2b8f05af5330412a06f350bdb1f0c
douglas86 <douglasmaxton@gmail.com>
2021-01-03 16:31:59 +0200

    Added handleSubmit to form.jsx

--- client/src/Components/Form.jsx
+++ client/src/Components/Form.jsx
@@ -1,9 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Form = () => {
+    const [list, setList] = useState('');
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+    };
+
+    console.log(list);
+
     return (
         <div>
             <h2>Form</h2>
+            <form onSubmit={handleSubmit}>
+                <input
+                    type="text"
+                    onChange={(e) => setList(e.target.value)}
+                    required
+                />
+                <button type="submit">Add</button>
+            </form>
         </div>
     );
 };
