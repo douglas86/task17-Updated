c76bcd36d4a54f69d9747022bb7d468aa48a8916
douglas86 <douglasmaxton@gmail.com>
2021-01-03 15:59:12 +0200

    Made changes & renamed App.jsx

---
+++ client/src/Components/App.jsx
@@ -0,0 +1,13 @@
+import '../extras/App.css';
+
+const App = () => {
+    return (
+        <div className="App">
+            <header className="App-header">
+                <p>Home Page</p>
+            </header>
+        </div>
+    );
+};
+
+export default App;
